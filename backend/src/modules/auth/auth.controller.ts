b8160fce7396ac99df71163d071d74c98e09db9d
Aakash Shrestha <aakash.cerestha90@gmail.com>
2026-06-22 13:46:25 +0545

    feat: implement user login functionality and enhance authentication services

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
-// import { LoginDto } from './dto/login.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,30 +11,10 @@ export class AuthController {
   signUp(@Body() registerDto: RegisterDto) {
     return this.authService.signUp(registerDto);
   }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
 }
-
-//  @Post('login')
-// login(@Body() loginDto: LoginDto) {
-//   return this.authService.create(loginDto);
-// }
-
-//   @Get()
-//   findAll() {
-//     return this.authService.findAll();
-//   }
-
-//   @Get(':id')
-//   findOne(@Param('id') id: string) {
-//     return this.authService.findOne(+id);
-//   }
-
-//   @Patch(':id')
-//   update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-//     return this.authService.update(+id, updateAuthDto);
-//   }
-
-//   @Delete(':id')
-//   remove(@Param('id') id: string) {
-//     return this.authService.remove(+id);
-//   }
-// }
